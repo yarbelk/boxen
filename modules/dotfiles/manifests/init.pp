f4df2088f9058d72183ae60c468514fe045c1f4b
James Rivett-Carnac <james.rivettcarnac@gmail.com>
2014-04-24 11:27:22 +0800

    switch dotfiles to oh-my-zsh - no idea if it will work

--- modules/dotfiles/manifests/init.pp
+++ modules/dotfiles/manifests/init.pp
@@ -4,30 +4,48 @@ $home = "Users/${::boxen_user}"
 $neo_dotfiles_dir = ${::boxen::config::srcdir}/neo_dotfiles"
 $gabe_dotfiles_dir = ${::boxen::config::srcdir}/gabe_dotfiles"
 
-repository { $neo_dotfiles_dir:
-  source => "neo/vim-config"
+$omz_repo = "robbyrussell_oh-my-zsh"
+$omz_path = "/Users/${::luser}/.oh-my-zsh"
+
+class ohmyzsh {
+  repository { $omz_repo,
+    source => 'robbyrussel/oh-my-zsh',
+    path => $omz_path
+  }
 }
 
-repository { $gabe_dotfiles_dir:
-  source => "gabehollombe/config_files"
+
+class dotfiles {
+  include "ohmyzsh"
+
+  file { "${home}/.tmux.conf":
+    ensure => link,
+    target => "${gabe_dotfiles_dir}/tmux.conf",
+    require => Repository[$gabe_dotfiles_dir]
+  }
+
+  exec { "install oh-my-zsh":
+    require => Repository["robbyrussell_oh-my-zsh"],
+    cwd => $omz_path,
+    provider => shell,
+    creates => "${home}/.zshrc",
+    command => "oh-my-zsh.sh && echo \"export LC_ALL=en_US.UTF-8\" >> ${home}/.zshrc && echo \"export LANG=en_US.UTF-8\" >> ${home}/.zshrc"
+
+  exec { "install vim config":
+    cwd => $neo_dotfiles_dir,
+    command => "rake",
+    provider => shell,
+    creates => [ "${home}/.vimrc", "${home}/.vim/" ],
+    require => Repository[$neo_dotfiles_dir]
+  }
+
+  repository { $neo_dotfiles_dir:
+    source => "neo/vim-config"
+  }
+
+  repository { $gabe_dotfiles_dir:
+    source => "gabehollombe/config_files"
+  }
+
 }
 
-file { "${home}/.zshrc":
-  ensure => link,
-  target => "${gabe_dotfiles_dir}/zshrc"
-  require => Repository[$gabe_dotfiles_dir]
-}
-
-file { "${home}/.tmux.conf":
-  ensure => link,
-  target => "${gabe_dotfiles_dir}/tmux.conf",
-  require => Repository[$gabe_dotfiles_dir]
-}
-
-exec { "install vim config":
-  cwd => $neo_dotfiles_dir,
-  command => "rake",
-  provider => shell,
-  creates => [ "${home}/.vimrc", "${home}/.vim/" ],
-  require => Repository[$neo_dotfiles_dir]
-}
